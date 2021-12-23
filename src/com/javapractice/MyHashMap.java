e9af77af6e76df5a1f3ff2e5efb2677c01ee6552
nikita <amarnikita09@gmail.com>
2021-12-23 13:24:03 +0530

    refactored MyHashMAp.java with better comments

--- src/com/javapractice/MyHashMap.java
+++ src/com/javapractice/MyHashMap.java
@@ -4,15 +4,19 @@ import java.util.HashMap;
 
 public class MyHashMap {
 	public static void main(String[] args) {
-		/* UC-1
+		/* UC-1 
 		 * Ability to find frequency of words in a sentence 
 		 * like “To be or not to be”
 		 */
 		String text = "to be or not to be";
 		String[] words = text.split(" ");
 	
-		
+		//empty hashmap created
 		HashMap<String, Integer> freqMap = new HashMap<>();
+		/*
+		 * adding the individual words as key if not present 
+		 * using else and updating count using if
+		 */
 		for(String word:words) {
 			 if(freqMap.containsKey(word)) {
 				 Integer count = freqMap.get(word);
